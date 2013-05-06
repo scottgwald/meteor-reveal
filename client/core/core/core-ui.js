bc0f0b1318d7b42b641fa24fd77c0ff11f264a55
Scott Greenwald <swgreen@mit.edu>
2013-05-06 02:03:40 -0400

    close menu upon link click + logger object to enable/disable console logging

--- client/core/core/core-ui.js
+++ client/core/core/core-ui.js
@@ -5,9 +5,29 @@ Template.menu_body.events({
 	}
 });
 
+Template.menu_body.created = function() {
+	console.log('hello 870907');
+
+	logger.disableLogger();
+	console.log('hi', 'hiya');
+	console.log('this wont show up in console');
+
+	logger.enableLogger();
+	console.log('This will show up!');
+
+	logger.disableLogger();
+}
+
 Template.menu_body.rendered = function() {
 	$('.my-menu-body').hide();
 }
+
+Template.menu.events({
+	'click a': function(events) {
+		console.log('clicked a link 809re.');
+		$('.my-menu-body').toggle();
+	}
+});
 
 // Template.menu.events({
 // 	'click ul>li>a': function() {
